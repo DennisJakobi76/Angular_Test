a4746e2213de5f7029e1d9611cc286800b728411
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-03-06 11:26:29 +0100

    added routing to imprint

--- src/app/main-content/landing-page/landingPage.component.ts
+++ src/app/main-content/landing-page/landingPage.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [NavbarComponent],
   template: `
     <section>
       <div>
         <app-navbar></app-navbar>
-        <router-outlet></router-outlet>
       </div>
 
       <h1 class="fontRaleway">SAKURA RAMEN</h1>
